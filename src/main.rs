98b248d20a765ac81f8c4c44b453bb141e904175
Johann Tuffe <tafia973@gmail.com>
2015-10-17 13:24:18 +0800

    fix image ratio and add perspective matrix

--- src/main.rs
+++ src/main.rs
@@ -49,10 +49,12 @@ fn load_atlas(atlas_src: &str, width: u32, height: u32, vertices: &mut Vec<Verte
     textures
 }
 
-fn draw_identity(texture_names: &HashMap<String, u32>, vertices: &mut Vec<Vertex>) -> Vec<u32> {
-
+fn draw_identity(texture_names: &HashMap<String, u32>, vertices: &mut Vec<Vertex>,
+    width: u32, height: u32) -> Vec<u32>
+{
+    let (width, height) = (width as f32, height as f32);
     let tex_to_pos = |t: &[f32; 2]| {
-        [2.0 * t[0] - 1.0, 2.0 * t[1] - 1.0]
+        [width * (2.0 * t[0] - 1.0), height * (2.0 * t[1] - 1.0)]
     };
 
     // Change all default position to texture position
@@ -64,10 +66,7 @@ fn draw_identity(texture_names: &HashMap<String, u32>, vertices: &mut Vec<Vertex
     // build shapes by manipulating indices
     // show all shapes for now
     let mut indices = Vec::with_capacity(texture_names.len() * 6);
-    // if let Some(n) = texture_names.get("ḧead") {
-    for (name, n) in texture_names.iter() {
-    // for n in texture_names.values() {
-        println!("text '{}'", &name);
+    for (_, n) in texture_names.iter() {
         indices.push(*n);
         indices.push(*n + 1);
         indices.push(*n + 2);
@@ -95,9 +94,7 @@ fn main() {
             (rgba.width(), rgba.height())
         }
     };
-    // let (width, height) = (1024, 256);
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
-    // let display = glium::glutin::WindowBuilder::new().with_dimensions(1024, 256).build_glium().unwrap();
     let texture = glium::texture::Texture2d::new(&display, image).unwrap();
 
     // load textures
@@ -105,12 +102,13 @@ fn main() {
     let texture_names = load_atlas(atlas_src, width, height, &mut vertices);
 
     // draw identity (for test)
-    let indices = draw_identity(&texture_names, &mut vertices);
+    let indices = draw_identity(&texture_names, &mut vertices, width, height);
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &vertices).unwrap();
     let indices = glium::IndexBuffer::new(&display,
                                           glium::index::PrimitiveType::TrianglesList,
                                           &indices).unwrap();
+
 
     // opengl vertex program
     let vertex_shader_src = r#"
@@ -118,11 +116,14 @@ fn main() {
 
         in vec2 position;
         in vec2 tex_coords;
+
         out vec2 v_tex_coords;
+
+        uniform mat2 perspective;
 
         void main() {
             v_tex_coords = tex_coords;
-            gl_Position = vec4(position, 0.0, 1.0);
+            gl_Position = vec4(perspective * position, 0.0, 1.0);
         }
     "#;
 
@@ -150,12 +151,21 @@ fn main() {
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 0.0);
 
-        let uniforms = uniform! {
-            tex: &texture,
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            // let aspect_ratio = height as f32 / width as f32 ;
+            [
+                [1.0 / width as f32, 0.0],
+                [0.0, 1.0 / height as f32]
+            ]
         };
 
-        target.draw(&vertex_buffer, &indices, &program, &uniforms,
-                    &Default::default()).unwrap();
+        let uniforms = uniform! {
+            tex: &texture,
+            perspective: perspective
+        };
+
+        target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
         target.finish().unwrap();
 
         for ev in display.poll_events() {
