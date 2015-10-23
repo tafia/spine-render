ef3f9014ec0722697e8b593dbdd4c039a8d2128c
Johann Tuffe <tafia973@gmail.com>
2015-10-23 09:12:54 +0800

    fix lot of things

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@
 extern crate glium;
 extern crate image;
 extern crate spine;
-extern crate cgmath;
 
 use std::collections::HashMap;
 use glium::{DisplayBuild, Surface};
 use glium::index::PrimitiveType;
 use spine::atlas::Atlas;
-use cgmath::Matrix4;
 
 use std::fs::File;
 use std::io::BufReader;
@@ -25,9 +23,7 @@ implement_vertex!(Vertex, position, tex_coords);
 
 /// insert all atlas textures into vertices
 /// returns attachment (name, index) defining 4 consecutive vertices
-fn load_atlas(atlas_src: &str, width: u32, height: u32,
-              vertices: &mut Vec<Vertex>,
-              indices: &mut Vec<u32>)
+fn load_atlas(atlas_src: &str, width: u32, height: u32, vertices: &mut Vec<Vertex>)
     -> HashMap<String, u32>
 {
     let (width, height) = (width as f32, height as f32);
@@ -48,20 +44,18 @@ fn load_atlas(atlas_src: &str, width: u32, height: u32,
         let tex2 = to_tex_coords(t.xy.0 + t.size.0, t.xy.1 + t.size.1);
         let tex3 = to_tex_coords(t.xy.0,            t.xy.1 + t.size.1);
         // get 4 vertices defining rectangle texture, centered per default
-        let (dx, dy) = (t.xy.0 as f32 / 2.0 / width, t.xy.1 as f32 / 2.0 / height);
-
-        vertices.push(Vertex { position: [-dx, dy], tex_coords: tex0 });
-        vertices.push(Vertex { position: [dx, dy], tex_coords: tex1 });
-        vertices.push(Vertex { position: [dx, -dy], tex_coords: tex2 });
-        vertices.push(Vertex { position: [-dx, -dy], tex_coords: tex3 });
+        if t.rotate {
+            vertices.push(Vertex { position: [0.0, 0.0], tex_coords: tex3 });
+            vertices.push(Vertex { position: [0.0, 0.0], tex_coords: tex0 });
+            vertices.push(Vertex { position: [0.0, 0.0], tex_coords: tex1 });
+            vertices.push(Vertex { position: [0.0, 0.0], tex_coords: tex2 });
+        } else {
+            vertices.push(Vertex { position: [0.0, 0.0], tex_coords: tex0 });
+            vertices.push(Vertex { position: [0.0, 0.0], tex_coords: tex1 });
+            vertices.push(Vertex { position: [0.0, 0.0], tex_coords: tex2 });
+            vertices.push(Vertex { position: [0.0, 0.0], tex_coords: tex3 });
+        }
         textures.insert(name, n);
-
-        indices.push(n);
-        indices.push(n + 1);
-        indices.push(n + 2);
-        indices.push(n + 2);
-        indices.push(n + 3);
-        indices.push(n);
 
         n += 4;
     }
@@ -70,27 +64,35 @@ fn load_atlas(atlas_src: &str, width: u32, height: u32,
 
 fn apply_sprite(sprites: &[spine::skeleton::animation::Sprite],
                 attachments: &HashMap<String, u32>,
-                vertices: &mut glium::VertexBuffer<Vertex>) {
+                vertices: &mut glium::VertexBuffer<Vertex>) -> Vec<u32> {
 
-    for (i, chunk) in vertices.map().chunks_mut(4).enumerate() {
-        // search corresponding sprite
-        if let Some(sprite) = attachments.iter().find(|&(_, n)| *n == 4*i as u32)
-                     .and_then(|(name, _)| sprites.iter()
-                        .find(|s| &s.attachment == name)) {
+    let mut indices = Vec::new();
+    let mut vertices = vertices.map();
+    for sprite in sprites {
+        // if &sprite.attachment == "head"
+        //     || &sprite.attachment == "goggles"
+        //     || &sprite.attachment == "eye_indifferent"
+        //     {
+        if let Some(&n) = attachments.get(&sprite.attachment) {
 
+            indices.push(n);
+            indices.push(n + 1);
+            indices.push(n + 2);
+            indices.push(n + 2);
+            indices.push(n + 3);
+            indices.push(n);
+
+            let n = n as usize;
             let positions = sprite.positions;
-            chunk[0].position[0] = positions[0][0];
-            chunk[0].position[1] = positions[0][1];
-            chunk[1].position[0] = positions[1][0];
-            chunk[1].position[1] = positions[1][1];
-            chunk[2].position[0] = positions[2][0];
-            chunk[2].position[1] = positions[2][1];
-            chunk[3].position[0] = positions[3][0];
-            chunk[3].position[1] = positions[3][1];
-            println!("modifying sprite '{}': {:?}", &sprite.attachment, chunk);
+            vertices[n].position = positions[0];
+            vertices[n + 1].position = positions[1];
+            vertices[n + 2].position = positions[2];
+            vertices[n + 3].position = positions[3];
+
+        // }
         }
     }
-
+    indices
 }
 
 // fn draw_identity(texture_names: &HashMap<String, u32>, vertices: &mut Vec<Vertex>,
@@ -125,9 +127,9 @@ fn apply_sprite(sprites: &[spine::skeleton::animation::Sprite],
 fn main() {
 
     // TODO: parse program arguments
-    let atlas_src = "/home/johann/projects/spine-render/example/spineboy.atlas";
-    let atlas_img = "/home/johann/projects/spine-render/example/spineboy.png";
-    let skeleton_src = "/home/johann/projects/spine-render/example/spineboy.json";
+    let atlas_src = "/home/johann/projects/spine_render/example/spineboy.atlas";
+    let atlas_img = "/home/johann/projects/spine_render/example/spineboy.png";
+    let skeleton_src = "/home/johann/projects/spine_render/example/spineboy.json";
 
     // prepare glium objects
     let image = image::open(atlas_img).expect("couldn't read atlas image");
@@ -141,28 +143,21 @@ fn main() {
     let window = glium::glutin::WindowBuilder::new().build_glium().unwrap();
     let texture = glium::texture::Texture2d::new(&window, image).unwrap();
 
-    println!("texture: {:?}", texture, );
-
     // load atlas textures
-    let (mut vertices, mut indices) = (Vec::new(), Vec::new());
-    let texture_names = load_atlas(atlas_src, width, height, &mut vertices, &mut indices);
+    let mut vertices = Vec::new();
+    let texture_names = load_atlas(atlas_src, width, height, &mut vertices);
+    let mut vertex_buffer = glium::VertexBuffer::new(&window, &vertices).unwrap();
 
     // load skeleton from json
     let f = File::open(skeleton_src).ok().expect("Cannot open json file");
     let reader = BufReader::new(f);
     let skeleton = spine::skeleton::Skeleton::from_reader(reader).ok().expect("error while parsing json");
-    let walk = skeleton.get_animated_skin("default", Some("walk")).ok().expect("error while creating animated skin");
 
-    let sprites = walk.interpolate(0.0).expect("default skin should not be empty");
+    let walk = skeleton.get_animated_skin("default", Some("walk")).ok().expect("error while creating animated skin");
+    // let walk = skeleton.get_animated_skin("default", None).ok().expect("error while creating animated skin");
 
     // draw identity (for test)
     // let indices = draw_identity(&texture_names, &mut vertices, width, height);
-
-    let mut vertex_buffer = glium::VertexBuffer::new(&window, &vertices).unwrap();
-    let indices = glium::IndexBuffer::new(&window, PrimitiveType::TrianglesList, &indices).unwrap();
-
-    // transform vertices
-    apply_sprite(&sprites, &texture_names, &mut vertex_buffer);
 
     // opengl vertex program
     let vertex_src = r#"
@@ -194,49 +189,51 @@ fn main() {
         }
     "#;
 
+    let params = glium::DrawParameters {
+        blend: glium::Blend::alpha_blending(),
+        .. Default::default()
+    };
+
+    let mut perspective = [[0.0, 0.0], [0.0, 0.0]];
+
     let program = glium::Program::from_source(&window, vertex_src, fragment_src, None).unwrap();
+    let delta = 0.001;
+    let mut iter = walk.iter(delta);
+    // let sprites = iter.next().unwrap();
 
     // the main loop, supposed to run with a constant FPS
     run::start_loop(|| {
 
-        let mut target = window.draw();
-        target.clear_color(0.0, 0.0, 1.0, 0.0);
+        if let Some(sprites) = iter.next() {
 
-        let perspective = {
+            let indices = apply_sprite(&sprites, &texture_names, &mut vertex_buffer);
+            let indices = glium::IndexBuffer::new(&window, PrimitiveType::TrianglesList, &indices).unwrap();
+
+            let mut target = window.draw();
+            target.clear_color(0.0, 0.0, 1.0, 0.0);
+
             let (width, height) = target.get_dimensions();
-            [
-                [1.0 / width as f32, 0.0],
-                [0.0, 1.0 / height as f32]
-            ]
-        };
+            perspective[0][0] = 1.0 / width as f32;
+            perspective[1][1] = 1.0 / height as f32;
 
-        let uniforms = uniform! {
-            tex: &texture,
-            perspective: perspective
-        };
+            let uniforms = uniform! {
+                tex: &texture,
+                perspective: perspective
+            };
 
-        let params = glium::DrawParameters {
-            blend: glium::Blend::alpha_blending(),
-            .. Default::default()
-        };
+            target.draw(&vertex_buffer, &indices, &program, &uniforms, &params).unwrap();
+            target.finish().unwrap();
 
-        target.draw(&vertex_buffer, &indices, &program, &uniforms, &params).unwrap();
-        target.finish().unwrap();
-
-        for ev in window.poll_events() {
-            match ev {
-                glium::glutin::Event::Closed => return run::Action::Stop,
-                _ => ()
+            for ev in window.poll_events() {
+                match ev {
+                    glium::glutin::Event::Closed => return run::Action::Stop,
+                    _ => ()
+                }
             }
+        } else {
+            iter = walk.iter(delta);
         }
 
         run::Action::Continue
     });
-
-        // for ev in window.wait_events() {
-        //     match ev {
-        //         glium::glutin::Event::Closed => return,
-        //         _ => ()
-        //     }
-        // }
 }
